cad3b59f3e93771fcaeb03b7b3a0f2c3f3de26f2
Vinit Tyagi <52591031+tyagivinit19@users.noreply.github.com>
2020-02-19 23:21:07 +0530

    Everything is working,almost i think

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,5 +5,16 @@ import javafx.event.ActionEvent;
 public class Controller {
 
     public void student(ActionEvent actionEvent) {
+
+    }
+
+    public void library(ActionEvent actionEvent) {
+    }
+
+    public void finance(ActionEvent actionEvent) {
+    }
+
+    public void admin(ActionEvent actionEvent) {
+
     }
 }
