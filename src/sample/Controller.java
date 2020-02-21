4fe9e5b4d9d2631ea864c37e414864cafe88db3f
Vinit Tyagi <52591031+tyagivinit19@users.noreply.github.com>
2020-02-21 10:55:04 +0530

    Scene is now working is now , I can change it smootly in same scene. The bug is i just forgot to declare right controller to the new fxmml file, so it just keep me error and take almost two days to find solution.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,20 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.fxml.FXMLLoader.load;
 
 public class Controller {
+
+    public AnchorPane mP;
 
     public void student(ActionEvent actionEvent) {
 
@@ -14,7 +26,12 @@ public class Controller {
     public void finance(ActionEvent actionEvent) {
     }
 
-    public void admin(ActionEvent actionEvent) {
+    public void admin(ActionEvent actionEvent) throws IOException, ClassNotFoundException {
 
+        Parent newroot = FXMLLoader.load(getClass().getResource("logIn.fxml"));
+
+        Stage stage = (Stage) mP.getScene().getWindow();
+        stage.setScene(new Scene(newroot));
+        stage.show();
     }
 }
